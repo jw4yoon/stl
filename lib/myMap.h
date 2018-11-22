ecb247e68029613267a291819e0aa7c38a1a3341
James Jae Won Yoon <JamesYoon@Jamess-Macbook-Pro.local>
2018-11-21 23:26:08 -0500

    - Added iterator to MyMap class - Need to figure out how to add elements to my map since find() or operator[] sh ould perform binary search(O(logn))

--- lib/myMap.h
+++ lib/myMap.h
@@ -9,38 +9,79 @@
 #ifndef myMap_h
 #define myMap_h
 
+#include <iterator>
+#include <initializer_list>
+
 #include "common.h"
 #include "myPair.h"
 #include "myVector.h"
 
 template<class Key, class Value>
 class MyMap {
-    MyVector<MyPair<Key, Value>> _myMap;
+    MyVector<MyPair<Key, Value>> _myVector;
 public:
     MyMap(){
         std::cout << "Basic constructor called" << std::endl;
     }
-    MyMap(const MyMap& other) : _myMap(other._myMap) {
+    explicit MyMap(std::initializer_list<MyPair<Key, Value>> il) {
+        std::cout << "Constructor with initializer_list" << std::endl;
+        for (auto elem : il) { // but need to sort this in order to perform O(logn) search
+            _myVector.push_back(elem);
+        }
+    }
+    MyMap(const MyMap& other) : _myVector(other._myVector) {
         std::cout << "Copy constructor called" << std::endl;
     }
     MyMap& operator=(const MyMap& other) {
         std::cout << "Copy assignment operator called" << std::endl;
         if (this != &other) {
-            _myMap = other._myMap;
+            _myVector = other._myVector;
         }
         return *this;
     }
-    MyMap(const MyMap&& other) : _myMap(std::move(other._myMap)) {
+    MyMap(const MyMap&& other) : _myVector(std::move(other._myVector)) {
         std::cout << "Move constructor called" << std::endl;
     }
     MyMap& operator=(const MyMap&& other) {
         std::cout << "Move assignment operator called" << std::endl;
         if (this != &other) {
-            _myMap = std::move(other._myMap);
+            _myVector = std::move(other._myVector);
         }
         return *this;
     }
-    
+    class Iterator {
+        MyVector<MyPair<Key, Value>>::Iterator _it;
+    public:
+        explicit Iterator(MyVector<MyPair<Key, Value>>::Iterator it): _it{it} {}
+        T& operator*() const {
+            return _it[curPos];
+        }
+        Iterator& operator++() {
+            ++_it;
+            return *this;
+        }
+        Iterator& operator++(int) {
+            return ++(*this);
+        }
+        bool operator==(const Iterator& other) {
+            return _it == other;
+        }
+        bool operator!=(const Iterator& other) {
+            return !(*this==other); // invoke operator==
+        }
+        Iterator operator+(int num) {
+            return _it+num;
+        }
+        Iterator operator-(int num) {
+            return _it-num;
+        }
+    };
+    Iterator begin() {
+        return Iterator{_myVector.begin()};
+    }
+    Iterator end() {
+        return Iterator{_myVector.end()};
+    }
     
     /*
     MyMap& operator[](Key key) {
